6f850e4f8c7bfcd0c525524cde30fd99fa1af32c
Unknown <10798368@my.uvu.edu>
2018-08-22 14:19:21 -0600

    ScriptableObjects demo

---
+++ DGM2670_StartProject/Assets/PrefabData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu]
+public class PrefabData : ScriptableObject 
+//Changed inheritance to ScriptableObject, in place of MonoBehaviour. 
+{
+    public GameObject Prefab;
+    public Sprite SpriteImage;
+    //Cannot name Sprite sprite, because it is a reserved data type.
+    public Color SpriteColor;
+
+    public void OnConfigure()
+    //Does not return a value.
+    {
+        var newPrefab = Instantiate(Prefab);
+        //What is it doing? What should its name be?
+        var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+        //GameObject does not come with SpriteRendered, so get the component.
+        newSprite.sprite = SpriteImage;
+        newSprite.color = SpriteColor;
+    }
+
+}
