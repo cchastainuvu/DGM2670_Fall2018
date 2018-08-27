9f03fbf4a2dd56c078e4b4657670e4d46e53216c
Unknown <10798368@my.uvu.edu>
2018-08-27 15:58:15 -0600

    unity events

---
+++ DGM2670_StartProject/Assets/EventScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+
+    public UnityEvent MyEvent;
+
+    private void OnMouseDown()
+    {
+        MyEvent.Invoke();
+        //Will run Prefab object on Start. Not dependent. Invoke > Run.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MyEvent.Invoke();
+    }
+}
