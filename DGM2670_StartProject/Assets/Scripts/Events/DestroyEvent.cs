36485b79c6277c9269b6fd0e2e6b3becfedc5554
Unknown <10798368@my.uvu.edu>
2018-08-29 14:04:17 -0600

    events review

---
+++ DGM2670_StartProject/Assets/Scripts/Events/DestroyEvent.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnDestroy()
+	{
+		Event.Invoke();
+	}
+}
---
+++ DGM2670_StartProject/Assets/Scripts/NewEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class NewEvent : MonoBehaviour
+{
+	public UnityEvent MyEvent;
+	//Unity Events are the same as the built in Unity Events "OnConfigure ()" on UI buttons.
+
+	private void OnMouseDown()
+	{
+		MyEvent.Invoke();
+		//Invoke is a method that is part of the Event.
+		//Will allow you to call any object, so long as it has a function.
+	}
+}
