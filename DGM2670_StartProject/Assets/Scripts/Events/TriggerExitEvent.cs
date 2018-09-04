d9da58e82cb4f4f4755bcf8c757d9cd9129ff332
Unknown <10798368@my.uvu.edu>
2018-09-04 01:17:57 -0600

    particles on/off bouncing ball

---
+++ DGM2670_StartProject/Assets/Scripts/Events/TriggerExitEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerExitEvent : MonoBehaviour
+{
+
+    public UnityEvent Event;
+
+    private void OnTriggerExit(Collider other)
+    {
+        Event.Invoke();
+    }
+
+
+}
