55ff8c3d7990bcdd3973c14b86b6370a126494b1
Unknown <10798368@my.uvu.edu>
2018-09-17 11:46:35 -0600

    MovePatternNotGrounded added

---
+++ DGM2670_StartProject/Assets/Scripts/FloatBool.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "FloatBool")]
+public class FloatBool : FloatInput 
+{
+
+     public override float Value
+    {
+         get
+         {
+
+             if (Input.GetButton(InputType))
+             {
+                return value;   
+             }
+                 return 0;
+             
+         }
+    }
+}
--- DGM2670_StartProject/Assets/Scripts/MovePattern.cs
+++ DGM2670_StartProject/Assets/Scripts/MovePattern.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MovePattern : ScriptableObject
 {
 
-    public FloatData jumpSpeed;
+//    public FloatData jumpSpeed;
     public FloatData gravity;
 
     public FloatData MoveX, MoveY, MoveZ;
@@ -16,25 +16,35 @@ public class MovePattern : ScriptableObject
     private Vector3 rotDirection;
 
 
-    public void Invoke(CharacterController controller, Transform transform)
+    public virtual void Invoke(CharacterController controller, Transform transform)
     {
-
         //Controller and Transform are red because we need to bring in MonoBehaviour.
 
         if (controller.isGrounded)
         {
+            Move(transform);
 
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            transform.Rotate(rotDirection);
-            rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-            moveDirection = transform.TransformDirection(moveDirection);
-
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed.Value;
-
+//            if (Input.GetButton("Jump"))
+//                moveDirection.y = jumpSpeed.Value;
         }
 
+            Move(controller);
+    }
+
+    protected void Move(CharacterController controller)
+    {
         moveDirection.y -= gravity.Value * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
+
+    protected void Move(Transform transform)
+    {
+        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+        transform.Rotate(rotDirection);
+        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+        moveDirection = transform.TransformDirection(moveDirection);
+    }
+    
+    //Both have the same name, but it is fine because they are different return types. (Method Overloading)
 }
+
