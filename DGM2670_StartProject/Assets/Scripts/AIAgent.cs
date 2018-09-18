d8348016193c25935abd38de523aea420d720c06
Unknown <10798368@my.uvu.edu>
2018-09-18 15:27:32 -0600

    Begin AI

---
+++ DGM2670_StartProject/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIAgent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Player;
+    public bool CanChase;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        
+        
+    }
+
+    private void Update()
+    {
+
+        if (true)
+        {
+             agent.destination = Player.position;
+        }
+        else
+        {
+            agent.destination = transform.position;
+            //Equal to self, essentially.
+        }
+       
+    }
+    
+    //Uppercase Transform is the CLASS. Lowercase transform is a variable hidden in MonoBehaviour.
+}
